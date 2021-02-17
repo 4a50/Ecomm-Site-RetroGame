6fa2d397a45475dfd22edfb3164b576a6299fe3a
4a50 <jpjones22@gmail.com>
2021-02-16 18:00:57 -0800

    Login Controller Built

---
+++ EcommerceApp/EcommerceApp/Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using EcommerceApp.Models.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceApp.Controllers
+{
+  public class LoginController : Controller
+  {
+    private IUserService userService;
+
+    public LoginController (IUserService service)
+    {
+      userService = service;
+    }
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Signup()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserDto>> Authenticate (LoginData data)
+    {
+      var user = await userService.Authenticate(data.Username, data.Password);
+      if (user == null)
+      {
+        return Redirect("/Login");
+      }
+      return Redirect("/Shop");
+    }
+    [HttpPost]
+    public async Task<ActionResult<UserDto>> Register (RegisterUser data)
+    {
+      data.Roles = new List<string>()
+      {
+        "guest"
+      };
+
+      var user = await userService.Register(data, this.ModelState);
+
+      if (ModelState.IsValid)
+      {
+        return Redirect("/Login");
+      }
+
+      return Redirect("/Register");
+    }
+    [Authorize]
+    public async Task<ActionResult<UserDto>> Profile()
+    {
+      UserDto user = await userService.GetUser(this.User);
+      return View(user);
+    }
+  }
+}
