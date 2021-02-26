b4363b4a9e5d1e96ed2b587818a31e1224107eaa
4a50 <jpjones22@gmail.com>
2021-02-26 15:26:31 -0800

    Adding logout

--- EcommerceApp/EcommerceApp/Pages/Index.cshtml.cs
+++ EcommerceApp/EcommerceApp/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
 using EcommerceApp.Models.Dto;
 using EcommerceApp.Models.Interfaces;
+using EcommerceApp.Models.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -9,11 +12,15 @@ namespace EcommerceApp.Pages
   public class IndexModel : PageModel
   {
     private IUserService userService;
+    private SignInManager<ApplicationUser> signInManager;
     public UserDto UserInfo { get; set; }
+    [BindProperty]
+    public bool IsSignedIn { get; set; }
 
-    public IndexModel(IUserService userserv)
+    public IndexModel(IUserService userserv, SignInManager<ApplicationUser> signIn)
     {
       userService = userserv;
+      signInManager = signIn;
     }
     public async Task OnGet()
     {
