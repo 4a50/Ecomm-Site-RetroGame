ab5686ce9d1926d027cbcf8e7bb2c467f0a6413e
4a50 <jpjones22@gmail.com>
2021-06-11 14:27:40 -0700

    Styled Game Detail page.  Fixed null Url

--- EcommerceApp/Pages/GameDetail/Index.cshtml.cs
+++ EcommerceApp/Pages/GameDetail/Index.cshtml.cs
@@ -33,6 +33,8 @@ namespace EcommerceApp.Pages.GameDetail
         try
         {
           Game = await GameRepository.GetGame(int.Parse(id));
+          if (Game.ImageUrl == "" || Game.ImageUrl == null) Game.ImageUrl = "https://via.placeholder.com/250";
+          Debug.WriteLine($"URL: {Game.ImageUrl}");
         }
         catch
         {
@@ -55,7 +57,7 @@ namespace EcommerceApp.Pages.GameDetail
      
       int gameid = Game.Id;      
       UserInfo = await userService.GetUser(this.User);      
-      string userid = UserInfo.Id;
+      string userid = UserInfo.Id == null ? "" : UserInfo.Id;
       var cart = await Cart.AddGameToCart(userid, gameid);
       if (cart == null)
       {
