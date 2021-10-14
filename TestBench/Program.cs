e94f46019f7f187d55c3780201489944c9c7ab79
4a50 <jpjones22@gmail.com>
2021-10-14 15:59:09 -0700

    retrive game and boxart data

--- TestBench/Program.cs
+++ TestBench/Program.cs
@@ -19,37 +19,67 @@ namespace TestBench
           GameID = (int)g["id"],
           Name = (string)g["game_title"],
           Description = (string)g["overview"],
-          ReleaseDate = (DateTime)g["release_date"]
-        });
-        Console.WriteLine(gameObjList[0].ID);
+          ReleaseDate = (DateTime)g["release_date"],
+          GameSystem = (string)g["platform"]
+        });        
       }
+
+      //populates the boxartUrls
+      UpdateBoxArt(jsonObj, gameObjList);
+      UpdateGameSystem(jsonObj, gameObjList);
+
       
-      Console.WriteLine(gamesList);
-
-
-      #region
-      //Console.WriteLine(gamesList);
-      //JObject includesItems = (JObject)jsonObj["include"]["boxart"]["data"];      
-      //foreach(JProperty p in includesItems.Properties())
-      //{
-      //  Console.WriteLine($"{p.Name}=> {p.Value}");
-
-      //}
-
-
-      //Console.WriteLine((string)jsonObj["include"]);
-
-
-      //JArray categories = (JArray)rss["channel"]["item"][0]["category"];
-      //Console.WriteLine(categories);
-      // [
-      //   "Json.NET",
-      //   "CodePlex"
-      // ]
-      #endregion
+      var findObj = gameObjList.Find((g) => g.Name == "Super Metroid");
+      Console.WriteLine(findObj);
     }
+  public static void UpdateBoxArt(JObject jsonObj, List<Game> gameObjList)
+  {
+      JObject includesItems = (JObject)jsonObj["include"]["boxart"]["data"];
+      string baseURLOrig = (string)jsonObj["include"]["boxart"]["base_url"]["original"];
+      string baseURLThumb = (string)jsonObj["include"]["boxart"]["base_url"]["thumb"];
+      //Creates an array from the "include.data"
+      foreach (JProperty p in includesItems.Properties())
+      {
+        //Find the object in List to update
+        Game gamUse = gameObjList.Find((g) => g.GameID == int.Parse(p.Name));
+        //if the object exists then proceed to find the values
+        if (gamUse != null)
+        {
+          //iterates through the object array for the ID key 
+          JArray jArray = (JArray)p.Value;
+          //determines the appropriate side of the image and updates the corresponding "Game" property.
+          foreach (JToken prop in jArray)
+          {
+            if ((string)prop["side"] == "back")
+            {
+              gamUse.BoxArtUrlBack = baseURLOrig + (string)prop["filename"];
+              gamUse.BoxArtUrlThumb = baseURLThumb + (string)prop["filename"];
+            }
+            if ((string)prop["side"] == "front")
+            {
+              gamUse.BoxArtUrlFront = baseURLOrig + (string)prop["filename"];
+              gamUse.BoxArtUrlThumb = baseURLThumb + (string)prop["filename"];
+            }
+          }
+        }
 
+      }
+    }
+  public static void UpdateGameSystem(JObject jsonObj, List<Game> gameObjList)
+  {
+      JObject platform = (JObject)jsonObj["include"]["platform"];
+      foreach(JProperty p in platform.Properties())
+      {
+        Game gameUse = gameObjList.Find(g => g.GameSystem == p.Name);
+        if (gameUse != null)
+        {          
+          gameUse.GameSystem = (string)p.Value["name"];
+        }
+      }
   }
+
+  }  
+
 
   class Game
   {
