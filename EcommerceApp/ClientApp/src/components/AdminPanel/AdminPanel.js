e52f9c8d173d1a094e3343e7ece3c20a430dfb1a
4a50 <jpjones22@gmail.com>
2021-11-24 11:09:27 -0800

    added flexibility to common modal buttons

--- EcommerceApp/ClientApp/src/components/AdminPanel/AdminPanel.js
+++ EcommerceApp/ClientApp/src/components/AdminPanel/AdminPanel.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
-import InputModal from './InputModal.js'
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import SelectedCard from './AdminItemCard.js';
-import EditInventoryCard from './EditInventoryCard.js'
+import EditInventoryCard from './ItemEdit/EditInventoryCard.js'
 import InventoryListGroup from './InventoryListGroup.js';
 import inventoryList from '../../sampleData/sampleServerResponse.json';
 import defaultObj from '../../data/noData.json'
